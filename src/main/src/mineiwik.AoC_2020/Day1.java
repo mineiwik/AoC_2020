e0f5ee468147237b4cd22907313ad89e1858d23f
mineiwik <29404520+mineiwik@users.noreply.github.com>
2020-12-01 11:08:46 +0100

    Improved day 1

--- src/main/src/mineiwik.AoC_2020/Day1.java
+++ src/main/src/mineiwik.AoC_2020/Day1.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.Collections;
 
 /*
  * Advent of Code 2020 - Day 1 - Report Repair
@@ -12,6 +13,7 @@ public class Day1 extends Day {
 
     private final String input;
     private final String[] lines;
+    private final int size;
     private ArrayList<Integer> numbers;
 
     Day1() throws IOException {
@@ -30,30 +32,46 @@ public class Day1 extends Day {
         for (String line : lines) {
             numbers.add(Integer.parseInt(line));
         }
+
+        Collections.sort(numbers);
+        size = numbers.size();
+
     }
 
     public String firstStar() {
-        // Some O(n^2) algorithm...
-        for (int i : numbers) {
-            for (int j : numbers) {
-                if (i + j == 2020) {
-                    return String.valueOf(i * j);
-                }
-            }
+        // EDIT: Improved this algorithm to have a complexity of O(n)
+        // numbers must be sorted (with merge sort the time complexity is O(n*log(n)))
+        // Therefore the total time complexity from sorting to finding the two numbers is O(n*log(n))
+
+        int left = 0;
+        int right = size - 1;
+
+        while(left < right){
+            int result = numbers.get(left) + numbers.get(right);
+            if (result == 2020) return String.valueOf(numbers.get(left) * numbers.get(right));
+            else if (result < 2020) left++;
+            else right--;
         }
         return "no solution found";
     }
 
     public String secondStar() {
-        // Some O(n^3) algorithm........
-        for (int i : numbers){
-            for (int j : numbers){
-                for (int k : numbers){
-                    if (i + j + k == 2020){
-                        return String.valueOf(i * j * k);
-                    }
-                }
+        // EDIT: Improved this algorithm to have a complexity of O(n^2)
+        // numbers must be sorted (with merge sort the time complexity is O(n*log(n)))
+        // Therefore the total time complexity from sorting to finding the two numbers is O(n^2)
 
+        int left, right;
+
+        for (int i = 0; i < numbers.size(); i++){
+
+            left = i + 1;
+            right = size - 1;
+
+            while(left < right){
+                int result = numbers.get(i) + numbers.get(left) + numbers.get(right);
+                if(result == 2020) return String.valueOf(numbers.get(i) * numbers.get(left) * numbers.get(right));
+                else if (result < 2020) left++;
+                else right--;
             }
         }
         return "no solution found";
