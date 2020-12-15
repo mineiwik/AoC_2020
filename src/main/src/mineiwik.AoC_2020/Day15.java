b3dd639a256d271f678dbd47d96049bb1616ffb3
mineiwik <29404520+mineiwik@users.noreply.github.com>
2020-12-15 09:42:56 +0100

    Improved Day 15

--- src/main/src/mineiwik.AoC_2020/Day15.java
+++ src/main/src/mineiwik.AoC_2020/Day15.java
@@ -2,7 +2,6 @@ package mineiwik.AoC_2020;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 
 /*
@@ -21,67 +20,32 @@ public class Day15 extends Day {
     }
 
     public String firstStar() {
-
-        HashMap<Integer, ArrayList<Integer>> memory = new HashMap<>();
-
-        int turn = 1;
-        int latest = 0;
-        int current;
-
-        while (turn < 2021) {
-            if (turn - 1 < starting.size()) {
-                memory.put(starting.get(turn - 1), new ArrayList<>(Collections.singletonList(turn)));
-                latest = starting.get(turn - 1);
-            }
-            else if (memory.containsKey(latest)){
-                if (memory.get(latest).size() == 1){
-                    current = 0;
-                }
-                else {
-                    current = memory.get(latest).get(memory.get(latest).size() - 1) - memory.get(latest).get(memory.get(latest).size() - 2);
-                }
-                if (memory.containsKey(current)) memory.get(current).add(turn);
-                else memory.put(current, new ArrayList<>(Collections.singletonList(turn)));
-                latest = current;
-            }
-            else memory.put(latest, new ArrayList<>(Collections.singletonList(turn - 1)));
-            turn++;
-        }
-
-        return String.valueOf(latest);
+        return String.valueOf(getLatestNumberSpoken(2020));
     }
 
     public String secondStar() {
+        // Less inefficient than before...
+        return String.valueOf(getLatestNumberSpoken(30000000));
+    }
 
-        // Totally inefficient!
-
-        HashMap<Integer, ArrayList<Integer>> memory = new HashMap<>();
+    private int getLatestNumberSpoken(int endTurn) {
+        HashMap<Integer, Integer> memory = new HashMap<>();
 
         int turn = 1;
-        int latest = 0;
-        int current;
 
-        while (turn < 30000001) {
-            if (turn - 1 < starting.size()) {
-                memory.put(starting.get(turn - 1), new ArrayList<>(Collections.singletonList(turn)));
-                latest = starting.get(turn - 1);
-            }
-            else if (memory.containsKey(latest)){
-                if (memory.get(latest).size() == 1){
-                    current = 0;
-                }
-                else {
-                    current = memory.get(latest).get(memory.get(latest).size() - 1) - memory.get(latest).get(memory.get(latest).size() - 2);
-                }
-                if (memory.containsKey(current)) memory.get(current).add(turn);
-                else memory.put(current, new ArrayList<>(Collections.singletonList(turn)));
-                latest = current;
-            }
-            else memory.put(latest, new ArrayList<>(Collections.singletonList(turn - 1)));
-            turn++;
+        for (Integer number : starting) {
+            memory.put(number, turn++);
         }
 
-        return String.valueOf(latest);
+        int latest = starting.get(starting.size() - 1);
+
+        while (turn <= endTurn) {
+            int newLatest = memory.containsKey(latest) ? turn - 1 - memory.get(latest) : 0;
+            memory.put(latest, turn - 1);
+            latest = newLatest;
+            turn++;
+        }
+        return latest;
     }
 
 }
